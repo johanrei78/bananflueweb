dab97f2432a46c28e4afcbfb45716b1dfb30cc3a
johanrei78 <johanrei78@gmail.com>
2026-08-30 19:20:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ function lagPForelder(ant, oye, kropp, vinge, kjonn) {
 
 
     return {
-        ant
+        anttenner:antenner,
         haplotyper: haplotyper,
         oyefargeX: oyefargeX,
         kjonn: kjonn
